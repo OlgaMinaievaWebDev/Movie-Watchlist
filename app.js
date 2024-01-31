d46a46e771e384b352f0ed3850fb82d571191cdf
Olga <minaeva9@gmail.com>
2024-01-31 17:11:14 -0500

    shows array of search

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ document.getElementById("search").addEventListener("click", function () {
   console.log(userInput.value);
   fetch(`http://www.omdbapi.com/?s=${userInput.value}&type=movie&apikey=861f8cc7`)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data.Search));
 });
