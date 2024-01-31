db2c8caa8065ef54b16abdc95d3130a1889348af
Olga <minaeva9@gmail.com>
2024-01-31 16:51:29 -0500

    search function modified

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let userInput = document.getElementById("userInput");
 
 document.getElementById("search").addEventListener("click", function () {
   console.log(userInput.value);
-  fetch(`http://www.omdbapi.com/?t=${userInput.value}&apikey=861f8cc7`)
+  fetch(`http://www.omdbapi.com/?s=${userInput.value}&type=movie&apikey=861f8cc7`)
     .then((res) => res.json())
     .then((data) => console.log(data));
 });
