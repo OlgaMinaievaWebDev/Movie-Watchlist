5ede9a2109d90902293ec6297ef5d241c01bc467
Olga <minaeva9@gmail.com>
2024-02-01 15:23:14 -0500

    logit for search added, css movie-card stylied

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 let userInput = document.getElementById("search");
+let feedHtml = "";
+const movieList = document.getElementById("movie-list-section");
 
-document.getElementById("search").addEventListener("click", function () {
-  console.log(userInput.value);
-  fetch(`http://www.omdbapi.com/?s=${userInput.value}&type=movie&apikey=861f8cc7`)
+document.getElementById("searchBtn").addEventListener("click", function () {
+  fetch(
+    `http://www.omdbapi.com/?s=${userInput.value}&type=movie&apikey=861f8cc7`
+  )
     .then((res) => res.json())
-    .then((data) => console.log(data.Search));
+    .then((data) => {
+      if (!data.Search) {
+        movieList.innerHTML = `
+     <p class="no-results">Unable to find what you're looking for. Please try another search.</p> 
+     `;
+        return;
+      } else {
+        movieList.innerHTML = `
+     <p class="results">Able to find what you're looking for. </p> 
+     `;
+        console.log(data.Search);
+      }
+    });
 });
