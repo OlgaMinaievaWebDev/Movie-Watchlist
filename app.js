b43beb3e7d4b7ebeeceacfc301da689d252b1f58
Olga <minaeva9@gmail.com>
2024-02-05 14:14:56 -0500

    button add, style css modified

--- app.js
+++ app.js
@@ -29,12 +29,20 @@ document.getElementById("searchBtn").addEventListener("click", function () {
         <div class="right">
           <div class="title">
             <h3>${data.Title}</h3>
+            <div class ="rating">
+            <i class="fa-solid fa-star" style="color: #ffd91a;"></i>
             <p>${data.imdbRating}</p>
+            </div>
           </div>
           <div class="middle-part">
             <p>${data.Runtime}</p>
             <p>${data.Genre}</p>
+            <div class="add-movie">
+            <button class="btn">
+              <i id="${data.imdbID}" class="fa-solid fa-circle-plus"></i>
+                        </button>
             <p>Whatchlist</p>
+            </div>
           </div>
           <div class="description">
             <p>${data.Plot}</p>
